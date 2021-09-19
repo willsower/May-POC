e6b0ba55453eef72cc381571719bb74dd1e5b63b
willsower <jatr812@gmail.com>
2021-09-18 22:33:03 -0400

    Almost have everything working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-ScrollReveal().reveal('.toast');
+ScrollReveal().reveal('.logo');
+ScrollReveal().reveal('.nav');
+
+ScrollReveal().reveal('.may_img', {delay: 100});
+ScrollReveal().reveal('.may_about', {delay: 100});
+
+ScrollReveal().reveal('.places_worked');
+ScrollReveal().reveal('.places_worked1', {delay: 100});
+ScrollReveal().reveal('.places_worked2', {delay: 200});
+ScrollReveal().reveal('.places_worked3', {delay: 300});
+ScrollReveal().reveal('.places_worked4', {delay: 400});
+ScrollReveal().reveal('.places_worked5', {delay: 500});
+
+ScrollReveal().reveal('.my_stats');
+ScrollReveal().reveal('.my_stats1', {delay: 100});
+ScrollReveal().reveal('.my_stats2', {delay: 200});
+ScrollReveal().reveal('.my_stats3', {delay: 300});
+ScrollReveal().reveal('.my_stats4', {delay: 400});
+
+ScrollReveal().reveal('.work_with_me');
+ScrollReveal().reveal('.email', {delay: 100});
+ScrollReveal().reveal('.phone', {delay: 100});
+ScrollReveal().reveal('.linkedin', {delay: 100});
+ScrollReveal().reveal('.instagram', {delay: 100});
+ScrollReveal().reveal('.my_form', {delay: 100});
 
 // Number Counter
 
